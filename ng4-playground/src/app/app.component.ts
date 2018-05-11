7fd8e2fcb6b9096fcbc6c6c97705f191f5b6676f
eraldcafka <erald.c@scopicsoftware.com>
2018-05-11 12:13:43 +0200

    + Modified AppComponent to use inline templating

--- ng4-playground/src/app/app.component.ts
+++ ng4-playground/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Hey from inline templating!</h1>
+    <p>Another line in this template</p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
